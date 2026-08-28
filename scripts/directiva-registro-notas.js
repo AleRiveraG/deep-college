1da83827dcb88ce58bc28af5fdcb37a5bfae66f7
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-08-28 09:44:48 -0400

    Incorporación JavaScript registro notas vista directiva

---
+++ scripts/directiva-registro-notas.js
@@ -0,0 +1,19 @@
+const cursos = document.querySelector('#curso');
+const resumenes = document.querySelector('.resumenes');
+
+let cursoActivo = null;
+
+cursos.addEventListener('change', ()=>{
+    resumenes.classList.remove('d-none')
+    
+    if(cursoActivo) {
+        cursoActivo.classList.add('d-none');
+    }
+    
+    const dato = '.'+'curso-'+cursos.value;
+    const curso = document.querySelector(dato);
+    curso.classList.remove('d-none');
+    cursoActivo = curso;
+
+})
+
