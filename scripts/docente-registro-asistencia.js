e590688f1c23d14313f5ee38adb9bb048cb42669
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-08-30 10:57:12 -0400

    Se agregaron más datos de prueba

--- scripts/docente-registro-asistencia.js
+++ scripts/docente-registro-asistencia.js
@@ -1,16 +1,29 @@
 const curso = document.querySelector('#curso');
-const tabla = document.querySelector('#tabla');
+const tabla = document.querySelector('#tabla-1');
+const tabla_2 = document.querySelector('#tabla-2');
+const tabla_3 = document.querySelector('#tabla-3');
 const botones = document.querySelectorAll('.botones');
 const registrar = document.querySelector('#registrar');
 const finalizar = document.querySelector('#finalizar');
 
 curso.addEventListener('change', (evento)=> {
     const opcion = evento.target.value;
-    tabla.classList.remove('d-none');
-
+    
     botones.forEach(boton => {
         boton.disabled = true;
     })
+
+    tabla.classList.add('d-none');
+    tabla_2.classList.add('d-none');
+    tabla_3.classList.add('d-none');
+
+    if(opcion === '1-medio' || opcion === '4-medio') {
+        tabla.classList.remove('d-none');
+    } else if (opcion === '2-medio') {
+        tabla_2.classList.remove('d-none');
+    } else {
+        tabla_3.classList.remove('d-none')
+    }
 })
 
 registrar.addEventListener('click', ()=> {
