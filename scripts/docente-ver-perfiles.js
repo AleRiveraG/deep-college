2106a553db84f4556ef92600728c2a524f5c4dc1
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-08-30 11:12:40 -0400

    Se agregaron más datos de prueba

--- scripts/docente-ver-perfiles.js
+++ scripts/docente-ver-perfiles.js
@@ -1,8 +1,20 @@
 const cursos = document.querySelector('#cursos');
 const tabla = document.querySelector('.tabla');
 
-cursos.addEventListener('change', ()=>{
-    tabla.classList.remove('d-none');
+cursos.addEventListener('change', (evento)=>{
+    const opcion = evento.target.value;
+
+    tabla.classList.add('d-none');
+    tabla_2.classList.add('d-none');
+    tabla_3.classList.add('d-none');
+
+    if(opcion === '1-medio' || opcion === '4-medio') {
+        tabla.classList.remove('d-none');
+    } else if (opcion === '2-medio') {
+        tabla_2.classList.remove('d-none');
+    } else {
+        tabla_3.classList.remove('d-none')
+    }
 })
 
 const busqueda = document.getElementById("busqueda");
