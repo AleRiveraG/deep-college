d83f411afe77a92f5005bef3917bf6a5f17feb84
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-08-31 13:44:37 -0400

    Corrección de errores

--- scripts/docente-ver-perfiles.js
+++ scripts/docente-ver-perfiles.js
@@ -1,15 +1,17 @@
 const cursos = document.querySelector('#cursos');
-const tabla = document.querySelector('.tabla');
+const tabla_1 = document.querySelector('#tabla-1');
+const tabla_2 = document.querySelector('#tabla-2');
+const tabla_3 = document.querySelector('#tabla-3');
 
 cursos.addEventListener('change', (evento)=>{
     const opcion = evento.target.value;
 
-    tabla.classList.add('d-none');
+    tabla_1.classList.add('d-none');
     tabla_2.classList.add('d-none');
     tabla_3.classList.add('d-none');
 
     if(opcion === '1-medio' || opcion === '4-medio') {
-        tabla.classList.remove('d-none');
+        tabla_1.classList.remove('d-none');
     } else if (opcion === '2-medio') {
         tabla_2.classList.remove('d-none');
     } else {
@@ -20,14 +22,21 @@ cursos.addEventListener('change', (evento)=>{
 const busqueda = document.getElementById("busqueda");
 busqueda.addEventListener('keyup', (evento)=> {
     const texto = evento.target.value.toLowerCase();
-    const filas = tabla.querySelectorAll('tbody tr');
 
-    filas.forEach((fila)=> {
-        const textoFila = fila.textContent.toLowerCase();
-        if(textoFila.includes(texto)) {
-            fila.style.display = '';
-        } else {
-            fila.style.display = 'none';
-        }
+    const tablas = [tabla_1, tabla_2, tabla_3];
+
+    tablas.forEach((tabla) =>{
+        const filas = tabla.querySelectorAll('tbody tr');
+    
+    
+
+        filas.forEach((fila)=> {
+            const textoFila = fila.textContent.toLowerCase();
+            if(textoFila.includes(texto)) {
+                fila.style.display = '';
+            } else {
+                fila.style.display = 'none';
+            }
+        });
     });
 });
